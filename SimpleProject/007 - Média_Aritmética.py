72f10a173ef24686359208c2eb8779c9d1e34607
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2017-12-27 23:10:49 -0300

    Add files via upload

---
+++ SimpleProject/007 - Média_Aritmética.py
@@ -0,0 +1,4 @@
+print('{:*^40}'.format(' Média Aritmética '))
+n1 = float(input('Digite o primeiro valor: '))
+n2 = float(input('Digite o segundo  valor: '))
+print('Nota1 = {}. Nota2 = {}. Média = {:.2f}.'.format(n1, n2, (n1 + n2)/2))
