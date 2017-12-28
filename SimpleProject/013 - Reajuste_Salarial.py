85bdf03df39867f65838520dac10c7552a86fb36
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2017-12-28 01:25:00 -0300

    Add files via upload

---
+++ SimpleProject/013 - Reajuste_Salarial.py
@@ -0,0 +1,3 @@
+print('{:*^40}'.format(' Reajuste Salarial '))
+salario = float(input('Digite o valor do salário. R$ '))
+print('O valor do salário R${} com aumento de 15% será R${:.2f}'.format(salario, salario * 1.15))
